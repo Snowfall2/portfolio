c741a125d93441f123bdb86fb8e6ce48fbe2432d
Snowfall <atittarn2@gmail.com>
2025-12-08 01:55:09 +0700

    Add metadata

--- src/components/resume/Project.tsx
+++ src/components/resume/Project.tsx
@@ -8,13 +8,13 @@ export default function Project() {
                 <li>
                     <SubHeader title="Codeforces Visualizer" urls={["https://github.com/Snowfall2/codeforces-visualizer", "https://cf-analysis.netlify.app/"]} beginDate={"November 2025"} endDate={"Present"}>
                         <ul className="ml-4 list-disc my-1">
-                            <li>Implement summary dashboard for user analysis using MUI</li>
-                            <li>Cache response for efficient data processing</li>
+                            <li>Implemented summary dashboard for user analysis using MUI</li>
+                            <li>Cached response for efficient data processing</li>
                         </ul>
                     </SubHeader>
                 </li>
                 <li>
-                    <SubHeader title="Find My Tutor" desc={"Website for students/tutors matching"} urls={["https://github.com/se2-9/frontend"]} beginDate={"January 2025"} endDate={"April 2025"}>
+                    <SubHeader title="Find My Tutor" desc={"Student-Tutor Matching Platform"} urls={["https://github.com/se2-9/frontend"]} beginDate={"January 2025"} endDate={"April 2025"}>
                         <ul className="ml-4 list-disc my-1">
                             <li>Implemented searching and filtering system using TanStack Query</li>
                             <li>Designed student and tutor home page for offer list using Next.js and shadcn library</li>
@@ -23,7 +23,7 @@ export default function Project() {
                     </SubHeader>
                 </li>
                 <li>
-                    <SubHeader title="Chatty" desc={"Website for group chat"} urls={["https://github.com/knwpound/network-client"]} beginDate={"March 2025"} endDate={"April 2025"}>
+                    <SubHeader title="Chatty" desc={"Real-time Group Chat Website"} urls={["https://github.com/knwpound/network-client"]} beginDate={"March 2025"} endDate={"April 2025"}>
                         <ul className="ml-4 list-disc my-1">
                             <li>Designed administrative interface for group chat using Next.js</li>
                             <li>Implemented real-time user presence detection using socket programming </li>
